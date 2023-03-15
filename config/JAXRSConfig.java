5b27598aa662221c80cccf0d7cb634aa39fb9896
Maciej Ciszewski <95685073+cichus91@users.noreply.github.com>
2023-03-15 20:38:10 +0100

    Delete config directory

--- config/JAXRSConfig.java
+++
@@ -1,9 +0,0 @@
-package pl.spjava.gabinet.config;
-
-import jakarta.ws.rs.ApplicationPath;
-import jakarta.ws.rs.core.Application;
-
-@ApplicationPath("gabinet")
-public class JAXRSConfig extends Application {
-
-}
